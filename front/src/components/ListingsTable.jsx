b8efbf4cd4ef2311f3a8ef6ea44c6b62792ca6ed
Blas Castro <castro.blas.martin@gmail.com>
2019-06-23 21:20:15 -0300

    highlight row

--- front/src/components/ListingsTable.jsx
+++ front/src/components/ListingsTable.jsx
@@ -25,6 +25,32 @@ class ListingsTable extends Component {
       this.props.get_properties(this.props.listings.previous)
   }
 
+  getTableRow = (row) => {
+    let highlight_style = parseInt(row.total_area) === 0 ?
+      {backgroundColor: 'red', color: 'white'} : {}
+    return (
+      <TableRow key={row.id}>
+        <TableCell component="th" scope="row" style={highlight_style}>
+          {row.id}
+        </TableCell>
+        <TableCell style={highlight_style}>{
+          row.location.length > 35 ?
+            row.location.substring(0, 35):
+            row.location
+         }</TableCell>
+        <TableCell align="right" style={highlight_style}>{row.price}</TableCell>
+        <TableCell style={highlight_style}>{row.subtype}</TableCell>
+        <TableCell style={highlight_style}>{
+            row.title.length > 35 ?
+            row.title.substring(0, 35):
+            row.title
+         }</TableCell>
+         <TableCell align="right" style={highlight_style}>{row.total_area}</TableCell>
+        <TableCell align="right" style={highlight_style}>{row.price_per_square}</TableCell>
+      </TableRow>
+    )
+  }
+
   render(){
     console.log(this.props.listings)
     let page = 0
@@ -51,25 +77,7 @@ class ListingsTable extends Component {
           </TableHead>
           <TableBody>
             {this.props.listings.results.map(row => (
-              <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell>{ 
-                  row.location.length > 35 ? 
-                    row.location.substring(0, 35):  
-                    row.location
-                 }</TableCell>
-                <TableCell align="right">{row.price}</TableCell>
-                <TableCell>{row.subtype}</TableCell>
-                <TableCell>{ 
-                    row.title.length > 35 ? 
-                    row.title.substring(0, 35):  
-                    row.title
-                 }</TableCell>
-                <TableCell align="right">{row.total_area}</TableCell>
-                <TableCell align="right">{row.price_per_square}</TableCell>
-              </TableRow>
+              this.getTableRow(row)
             ))}
           </TableBody>
           <TableFooter>
